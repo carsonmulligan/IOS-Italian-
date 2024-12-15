1c37dd02a7b7eb58df25b887b66a414b85101e3f
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2024-12-15 12:56:35 -0500

    Initial Commit

---
+++ TestAI/TestAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAIApp.swift
+//  TestAI
+//
+//  Created by Carson Mulligan on 12/15/24.
+//
+
+import SwiftUI
+
+@main
+struct TestAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
